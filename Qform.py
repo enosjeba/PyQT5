4d08c7ac2996b04c1e962c1558e038a01d3a13da
Enosjeba <enosjeba@gmail.com>
2022-11-18 22:27:04 +0530

    Update Qform.py

--- Qform.py
+++ Qform.py
@@ -20,9 +20,23 @@ class MainWindow(qtw.QWidget):
 
         #Stuff/Widget
         label_1 = qtw.QLabel("Cool Label Row")
-        
+        label_1.setFont(qtg.QFont("Helvetica", 24))
+
+        f_name = qtw.QLineEdit(self)
+        l_name = qtw.QLineEdit(self)
+
+        #Add rows to app
+        form_layout.addRow(label_1)
+        form_layout.addRow("First Name", f_name)
+        form_layout.addRow("Last Name", l_name)
+        form_layout.addRow(qtw.QPushButton("Click Me",
+        clicked = lambda: press_it()))
+
 
         self.show()
+        
+        def press_it():
+            label_1.setText(f'You Clicked the Button, {f_name.text()}')
 
 
 app = qtw.QApplication([])
