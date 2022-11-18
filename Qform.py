5707047783a69bb6972482d0386662d646df1fd1
Enosjeba <enosjeba@gmail.com>
2022-11-18 22:23:05 +0530

    Update Qform.py

--- Qform.py
+++ Qform.py
@@ -18,40 +18,9 @@ class MainWindow(qtw.QWidget):
         my_label.setFont(qtg.QFont('Helvetica', 18))
         self.layout().addWidget(my_label)
 
-        #Create A Entry Box
-        my_entry = qtw.QLineEdit()
-        my_entry.setObjectName("name_field")
-        my_entry.setText("Enter Project Name")
-        self.layout().addWidget(my_entry)
-
-        #Create a Text Box
-        my_text = qtw.QTextEdit(self,
-
-        plainText = "Real Text",
-        acceptRichText = True, 
-        lineWrapMode = qtw.QTextEdit.FixedColumnWidth,
-        lineWrapColumnOrWidth = 50,
-        placeholderText = "Type Here",
-        readOnly = False,
-
-        )
-
-        #Change Font Size of Spin Box
-        #my_spin.setFont(qtg.QFont('Helvetica',14))
-
-        #Put Combo Box on Screen
-        self.layout().addWidget(my_text)
-    
-        #Create a Button
-        my_button = qtw.QPushButton("Create Project", 
-        clicked = lambda: press_it())
-        self.layout().addWidget(my_button)
-
-        def press_it():
-            my_label.setText(f'{my_text.toPlainText()} typed')
-            my_text.setPlainText("You Pressed the Button!")
-            #clear entry box
-            my_entry.setText("")
+        #Stuff/Widget
+        label_1 = qtw.QLabel("Cool Label Row")
+        
 
         self.show()
 
